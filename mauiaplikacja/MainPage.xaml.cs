abccab12c526e434c59ef72206a6e851501d6825
sajmonn1 <szym00noff@gmail.com>
2024-09-16 09:26:24 +0200

    kostki

--- mauiaplikacja/MainPage.xaml.cs
+++ mauiaplikacja/MainPage.xaml.cs
@@ -1,25 +1,43 @@
-﻿namespace DiceRoll
+﻿namespace mauiaplikacja
 {
     public partial class MainPage : ContentPage
     {
-
-
         public MainPage()
         {
             InitializeComponent();
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
+private void OnCounterClicked(object sender, EventArgs e)
         {
-            //wygenerowanie liczby losowej
+            // Tworzenie obiektu Random
             Random r = new Random();
-            //genrujemy rzut kostką K6 - <1;6>
-            int diceRoll = r.Next(1, 7);
+            int diceRoll = 0;
 
-            CounterBtn.Text = "Wysolowany rzut: " + diceRoll.ToString();
+            // Sprawdzenie, który RadioButton jest zaznaczony
+            if (Dice4RadioButton.IsChecked)
+            {
+                // Rzut kostką K4
+                diceRoll = r.Next(1, 5);
+            }
+            else if (Dice6RadioButton.IsChecked)
+            {
+                // Rzut kostką K6
+                diceRoll = r.Next(1, 7);
+            }
+            else if (Dice10RadioButton.IsChecked)
+            {
+                // Rzut kostką K10
+                diceRoll = r.Next(1, 11);
+            }
+            else if (Dice12RadioButton.IsChecked)
+            {
+                // Rzut kostką K12
+                diceRoll = r.Next(1, 13);
+            }
+
+            // Wyświetlenie wyniku
+            RollValueLabel.Text = "Wynik rzutu: " + diceRoll.ToString();
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
-
 }
